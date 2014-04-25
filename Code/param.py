258fff24aa4a67e70ac6e659a152d3ef0d948f46
Green <aag2@688sdmac2.bw.nist.gov>
2014-04-24 18:26:06 -0600

    Started on the splitstep method. Have a semi working model based off the matlab code in the book.

---
+++ Code/param.py
@@ -0,0 +1,30 @@
+import math
+import numpy as np
+#Input Parameters
+distance=10
+beta2=1
+N=2
+mshape=0
+chirp0=0
+
+#Simulation Parameters
+nt=1024
+Tmax=32
+stepNum=round(20.*distance*N**2.)
+deltaz=distance/stepNum
+dtau=(2.*Tmax)/nt
+
+##Arrays
+#
+#tau =dtau*np.arange(0,nt)
+#omega = math.pi/Tmax*np.linspace(-nt/2,nt/2, num=nt)
+#
+#
+##Input Field Profile
+#
+#if mshape == 0:
+#    uu=np.cosh(tau)**-1*np.exp(-0.5j*chirp0*tau**2) #soliton
+#else:
+#    uu = np.exp(-0.5*(1+1j*chirp0)*tau**(2*mshape))
+#
+#
--- Code/ssnsl.py
+++ Code/ssnsl.py
@@ -1,9 +1,57 @@
 import sys
 import os
-from numpy import *
+import numpy as np
 import math
-
+import param
+import matplotlib.pyplot as plt
 '''This program will do it's best to compute the split-step 
 fourier transform to model nonlinear optical pulse propogation'''
 
+if __name__ == '__main__':
+    
+    #Arrays
 
+    tau =param.dtau*np.arange(-param.nt/2,param.nt/2)
+    omega = math.pi/param.Tmax*np.linspace(-param.nt/2,param.nt/2, num=param.nt)
+
+
+    #Input Field Profile
+
+    if param.mshape == 0:
+        uu=np.cosh(tau)**-1*np.exp(-0.5j*param.chirp0*tau**2) #soliton
+    else:
+        uu = np.exp(-0.5*(1+1j*param.chirp0)*tau**(2*param.mshape))
+
+    
+    #Plot Spectrum
+    spec =np.fft.ifftshift( np.fft.fft(uu))
+    freq = np.fft.ifftshift(np.fft.fftfreq(param.nt,d=param.dtau))
+    f = plt.figure()
+    plt.subplot(2,1,1)
+    plt.plot(tau, np.absolute(uu)**2, 'ko-') 
+    plt.title('Power of Input Pulse')
+    plt.xlabel('Time')
+    plt.subplot(2,1,2)
+    plt.plot(np.fft.ifftshift(np.fft.fftfreq(param.nt,d=param.dtau)), np.absolute(spec)**2, 'ko-')
+    plt.title('Power of Spectrum')
+    plt.xlabel('Frequency')
+    
+    #Store Dispersion
+    dispersion = np.exp(1j*0.5*param.beta2*freq**2*param.deltaz)
+    hhz = 1j*N**2*param.deltaz
+
+    #use symmetrized split step method
+
+    temp =uu*np.exp(np.absolute(uu)**2*hhz/2)
+    for x in range(param.stepNum):
+        fTemp= np.fft.fft(temp)*dispersion
+        uu = np.fft.ifft(fTemp)
+        temp= uu*np.exp(np.absolute(uu)**2*hhz)
+    uu = temp*np.exp(-np.absolute(uu)**2*hhz/2) #Final Field
+    temp = np.fft.fftshift(np.fft.fft(uu)*(param.nt*dtau)/math.sqrt(2*math.pi)
+
+    figure(2)
+    subplot(2,1,1)
+    plt.plot(tau, np.absolute(uu)**2, 'ko-')
+    subplot(2,1,2)
+    plt.plot(freq, np.absolute(temp)**2, 'ko-')
